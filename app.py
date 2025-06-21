e1fe9264e406925445182eebb5ad9ad57013f8e1
simon777333777 <zonoskrym711023@gmail.com>
2025-06-21 23:47:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ API_KEY = st.secrets["API_KEY"]
 st.markdown("""
     <style>
     html, body, [class*="css"]  {
-        font-size: 14px;
+        font-size: 8px;
     }
     </style>
 """, unsafe_allow_html=True)
